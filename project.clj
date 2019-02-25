472a2b328907f9ea231348778b79e293838b7fb1
conao3 <conao3@gmail.com>
2019-02-26 05:44:15 +0900

    generate lein project

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject lambda-helper "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure       "1.9.0"]
+                 [org.clojure/clojurescript "1.10.312"]
+                 [io.nervous/cljs-lambda    "0.3.5"]]
+  :plugins [[lein-npm                    "0.6.2"]
+            [io.nervous/lein-cljs-lambda "0.6.6"]]
+  :npm {:dependencies [[serverless-cljs-plugin "0.1.2"]]}
+  :cljs-lambda {:compiler
+                {:inputs  ["src"]
+                 :options {:output-to     "target/lambda-helper/lambda_helper.js"
+                           :output-dir    "target/lambda-helper"
+                           :target        :nodejs
+                           :language-in   :ecmascript5
+                           :optimizations :simple}}})
---
+++ src/lambda_helper/core.cljs
@@ -0,0 +1,7 @@
+(ns lambda-helper.core
+  (:require [cljs-lambda.macros :refer-macros [defgateway]]))
+
+(defgateway echo [event ctx]
+  {:status  200
+   :headers {:content-type (-> event :headers :content-type)}
+   :body    (event :body)})
