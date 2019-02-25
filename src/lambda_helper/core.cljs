561b084b15cea9c5b3a02621df37a96d022ae930
conao3 <conao3@gmail.com>
2019-02-26 07:22:53 +0900

    add hello-query function

--- src/lambda_helper/core.cljs
+++ src/lambda_helper/core.cljs
@@ -10,3 +10,9 @@
   {:status 200
    :headers {:content-type (-> event :headers :content-type)}
    :body "hello world"})
+
+(defgateway hello-query [event ctx]
+  (let [name (get-in event [:query :name])]
+    {:status 200
+     :headers {:content-type "application/json"}
+     :body (JSON/stringify (clj->js {:hello (or name "world")}))}))
