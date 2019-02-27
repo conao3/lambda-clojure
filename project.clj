0c23bbf06a62648f0d9e640aff350da9f94ee642
conao3 <conao3@gmail.com>
2019-02-27 13:10:46 +0900

    add figwheel dependency

--- project.clj
+++ project.clj
@@ -5,8 +5,22 @@
                  ;; [clj-http "3.9.1"]
                  [cljs-http "0.1.46"]]
   :plugins [[lein-npm                    "0.6.2"]
-            [io.nervous/lein-cljs-lambda "0.6.6"]]
+            [io.nervous/lein-cljs-lambda "0.6.6"]
+            [lein-figwheel "0.5.18"]]
   :npm {:dependencies [[serverless-cljs-plugin "0.1.2"]]}
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src"]
+                        :figwheel true
+                        :compiler {:main lambda-helper.core
+                                   :output-to     "target/lambda-helper/lambda_helper.js"
+                                   :output-dir    "target/lambda-helper"
+                                   :target :nodejs
+                                   :optimizations :none
+                                   :source-map-timestamp true}}]}
+  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.18"]
+                                  [com.cemerick/piggieback "0.2.2"]]
+                   :source-paths ["src" "dev"]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
   :cljs-lambda {:compiler
                 {:inputs  ["src"]
                  :options {:output-to     "target/lambda-helper/lambda_helper.js"
