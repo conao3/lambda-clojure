dd1d8dd12b0fb012958b1742f9ab250fa7889979
conao3 <conao3@gmail.com>
2019-03-05 02:41:50 +0900

    clean code

--- src/lambda_clojure/core.clj
+++ src/lambda_clojure/core.clj
@@ -1,31 +1,13 @@
 (ns lambda-clojure.core
-  (:gen-class
-   :name lambdaClojure
-   :methods [^:static [handler [String] String]]))
-
-(defn -handler [s]
-  (str "Hello " s "!"))
-
-; Add POJO handling
-
-(defn -handlepojo [this event]
-  (str "Hello " (.getFirstName event) " " (.getLastName event)))
-
-(gen-class
-  :name PojoHandler
-  :methods [[handlepojo [example.MyEvent] String]])
-
-;;
+  (:gen-class))
 
 (defn simple-lambda [event]
-  ;; "{ \"headers\": { \"content-type\": null },\"body\": \"hello world\",\"statusCode\": 200 }"
   {'statusCode 200
    'headers {'content-type (-> event :headers :content-type)}
    'body "hello world"
    })
 
 (defn simple-lambda-body [event]
-  ;; "{ \"headers\": { \"content-type\": null },\"body\": \"hello world\",\"statusCode\": 200 }"
   {'statusCode 200
    'headers {'content-type (-> event :headers :content-type)}
    'body (JSON/stringify {'message "hello world"})
