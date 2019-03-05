4c29a4e1bd1a930db25d12ee2b2bfd89c63154c2
conao3 <conao3@gmail.com>
2019-03-05 12:22:03 +0900

    clarify require clojure.data.json

--- src/lambda_clojure/core.clj
+++ src/lambda_clojure/core.clj
@@ -2,14 +2,21 @@
   (:require [clojure.data.json :as json])
   (:gen-class))
 
-(defn hello [event]
+(defn hello-simple [event]
   {'statusCode 200
-   'headers {'content-type (-> event :headers :content-type)}
+   'headers {'content-type "application/json"}
    'body "hello world"
    })
 
-(defn hello-json [event]
+(defn hello-json-without-require [event]
   {'statusCode 200
-   'headers {'content-type (-> event :headers :content-type)}
-   'body (json/write-str {'message "hello world"})
+   'headers {'content-type "application/json"}
+   'body (json/write-str {:message "hello world"})
+   })
+
+(defn hello-json [event]
+  (require '[clojure.data.json :as json])
+  {'statusCode 200
+   'headers {'content-type "application/json"}
+   'body (json/write-str {:message "hello world"})
    })
