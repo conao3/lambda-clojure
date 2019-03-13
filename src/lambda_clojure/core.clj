e9622986e7aec0c9c7080465403de731daecde1f
conao3 <conao3@gmail.com>
2019-03-14 05:00:45 +0900

    add cammel function

--- src/lambda_clojure/core.clj
+++ src/lambda_clojure/core.clj
@@ -3,6 +3,8 @@
   (:gen-class
    :methods [^:static
              [json [Object] Object]
+             ^:static
+             [json_without_require_snake [Object] Object]
              ;; [hello-json-without-require [String] String]
              ]))
 
@@ -17,6 +19,27 @@
    'headers {'content-type "application/json"}
    'body (json/write-str {:message "hello world"})
    })
+
+(defn -jsonWithoutRequireCammel [event]
+  {'statusCode 200
+   'headers {'content-type "application/json"}
+   'body (json/write-str {:message "hello world"})
+   })
+
+(gen-class
+ :name lambda_clojure.core
+ :methods [[jsonWithoutRequireCammel [Object] Object]])
+
+(defn json-without-require-snake [this event]
+  {'statusCode 200
+   'headers {'content-type "application/json"}
+   'body (json/write-str {:message "hello world"})
+   })
+
+(gen-class
+ :name lambda_clojure.json_without_require
+ :prefix json-without-require-
+ :methods [[snake [Object] Object]])
 
 (defn hello-json [event]
   (require '[clojure.data.json :as json])
