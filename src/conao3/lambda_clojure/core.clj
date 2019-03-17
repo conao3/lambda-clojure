d51cecfe6ab724383d4ba78852d6237930e06b90
conao3 <conao3@gmail.com>
2019-03-17 18:21:29 +0900

    add hello-html method

--- src/conao3/lambda_clojure/core.clj
+++ src/conao3/lambda_clojure/core.clj
@@ -8,7 +8,13 @@
 (defn hello-args [req]
   (response {:rest (print-str req)}))
 
+(defn hello-html [req]
+  (-> (response "html")
+      (response/content-type "text/html; charset=utf-8")))
+
 (defroutes hello-routes
-  (GET "/hello" _ (response {:message "Hello World"}))
-  (GET "/hello-fn" _ hello-fn)
-  (GET "/hello-args/:name" _ hello-args))
+  (context "/hello" _
+           (GET "/" _ (response {:message "Hello World"}))
+           (GET "/fn" _ hello-fn)
+           (GET "/args/:name" _ hello-args)
+           (GET "/html" _ hello-html)))
