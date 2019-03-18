2ac3ca145f4a1a30b12e5e941bf786f50f368fc9
conao3 <conao3@gmail.com>
2019-03-18 12:50:56 +0900

    add header-png method

--- src/conao3/lambda_clojure/core.clj
+++ src/conao3/lambda_clojure/core.clj
@@ -1,13 +1,13 @@
 (ns conao3.lambda-clojure.core
   (:require [compojure.core :refer :all]
             [clojure.string :as string]
-            [ring.util.response :as response :refer [response]]))
+            [ring.util.response :as response :refer [response]]
+            [dali.io :as io]
+            [dali.batik :as batik]))
 
-(defn header [{:keys [params]}]
-  (let [{:keys [str forground background]
-         :or {str "Unknown" forground "FFF" background "222"}}
-        params]
-    (-> (string/join
+(defn github-svg [{:keys [str forground background]
+                   :or {str "Unknown" forground "FFF" background "222"}}]
+  (-> (string/join
          "\n" ["<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"1000\" height=\"170\">"
                "<style xmlns=\"http://www.w3.org/2000/svg\" type=\"text/css\">"
                "@import url('https://fonts.googleapis.com/css?family=Sarabun:100');"
@@ -20,9 +20,21 @@
                "<text x=\"90\" y=\"120\">%s</text>"
                "</g>"
                "</svg>"])
-        (format background forground str)
-        (response)
-        (response/content-type "image/svg+xml; charset=utf-8"))))
+        (format background forground str)))
+
+(defn header [{:keys [params]}]
+  (-> (github-svg params)
+      (response)
+      (response/content-type "image/svg+xml; charset=utf-8")))
+
+(defn header-png [{:keys [params]}]
+  (-> (github-svg params)
+      (batik/parse-svg-string)
+      (batik/render-document-to-png "/tmp/hello-world.png" {}))
+  (-> (slurp "/tmp/hello-world.png")
+      (response)
+      (response/content-type "image/png")))
 
 (defroutes core-routes
-  (GET "/header/:str.svg" _ header))
+  (GET "/header/:str.svg" _ header)
+  (GET "/header/:str.png" _ header-png))
